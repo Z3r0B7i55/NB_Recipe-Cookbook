fa393012a3b93fe571daba95dad1f4e58fd17c9b
Charlie H. <charles_hopkins@me.com>
2022-08-19 15:26:16 -0500

    Wrote the foundation for Recipe_Box.java and included placeholders.

--- src/main/java/com/mycompany/project_cookbook/Recipe_Box.java
+++ src/main/java/com/mycompany/project_cookbook/Recipe_Box.java
@@ -3,11 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.project_cookbook;
-
+import java.util.ArrayList;
 /**
  *
  * @author Charl
  */
 public class Recipe_Box {
+    private ArrayList<Recipe> listOfRecipes;
     
+    public ArrayList<Recipe> getListOfRecipes() {
+        return listOfRecipes;
+    }
+    
+    public void setListOfRecipes(ArrayList<Recipe> recipes) {
+        listOfRecipes = recipes;
+    }
+    
+    public void Recipe_Box() {
+        listOfRecipes = new ArrayList<Recipe>();
+    }
+    
+    public void printAllRecipeDetails(String recipe) {
+        System.out.println("FIXME: printAllRecipeDetails() - Recipe_Box.java");
+    }
+    
+    public void printAllRecipeNames() {
+        System.out.println("FIXME: printAllRecipeNames() - Recipe_Box.java");
+    }
+    
+    public void addNewRecipe() {
+        System.out.println("FIXME: addNewRecipe() - Recipe_Box.java");
+    }
 }
