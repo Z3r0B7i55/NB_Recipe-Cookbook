cc4dfa783a931c73c853afc9c46f3c5a4a60ec92
Charlie H. <charles_hopkins@me.com>
2022-08-19 15:47:05 -0500

    Added Recipe_Box.java comments.

--- src/main/java/com/mycompany/project_cookbook/Recipe_Box.java
+++ src/main/java/com/mycompany/project_cookbook/Recipe_Box.java
@@ -5,33 +5,33 @@
 package com.mycompany.project_cookbook;
 import java.util.ArrayList;
 /**
- *
- * @author Charl
+ * @data 8/19/2022
+ * @author Charlie H.
  */
 public class Recipe_Box {
-    private ArrayList<Recipe> listOfRecipes;
+    private ArrayList<Recipe> listOfRecipes;                    // Stores all Recipe Box recipes.
     
-    public ArrayList<Recipe> getListOfRecipes() {
+    public ArrayList<Recipe> getListOfRecipes() {               // Accessor for the list of recipes.
         return listOfRecipes;
     }
     
-    public void setListOfRecipes(ArrayList<Recipe> recipes) {
+    public void setListOfRecipes(ArrayList<Recipe> recipes) {   // Mutator for the list of recipes.
         listOfRecipes = recipes;
     }
     
-    public void Recipe_Box() {
+    public void Recipe_Box() {                                  // Constructor for the Recipe_Box.
         listOfRecipes = new ArrayList<Recipe>();
     }
     
-    public void printAllRecipeDetails(String recipe) {
+    public void printAllRecipeDetails(String recipe) {          // Prints Recipe Ingredients, servings etc.
         System.out.println("FIXME: printAllRecipeDetails() - Recipe_Box.java");
     }
     
-    public void printAllRecipeNames() {
+    public void printAllRecipeNames() {                         // Prints all recipes by name. 
         System.out.println("FIXME: printAllRecipeNames() - Recipe_Box.java");
     }
     
-    public void addNewRecipe() {
+    public void addNewRecipe() {                                // Adds new recipes to recipe box.
         System.out.println("FIXME: addNewRecipe() - Recipe_Box.java");
     }
 }
