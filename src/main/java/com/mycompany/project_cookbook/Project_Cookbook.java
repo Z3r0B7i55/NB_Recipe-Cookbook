6d1615e8e3508c9e51be1403f353d6de77960475
Charlie H. <charles_hopkins@me.com>
2022-08-23 13:43:22 -0500

    Added temporary code to access Ingredient.java.

--- src/main/java/com/mycompany/project_cookbook/Project_Cookbook.java
+++ src/main/java/com/mycompany/project_cookbook/Project_Cookbook.java
@@ -6,12 +6,44 @@
 
 package com.mycompany.project_cookbook;
 
-import java.util.Scanner;   // For gathering user input for the menu.
-import java.util.ArrayList; // May not need this one, too soon to tell.
+import java.util.Scanner;   
+import java.util.ArrayList; 
+import java.util.Arrays;  
+import java.util.InputMismatchException;
 
 public class Project_Cookbook {
     
-    public static void main(String[] args) {
+    public int runMenu() {
+        int callback;
+        boolean exit = false;
+        Scanner userInput = new Scanner(System.in);
+        ArrayList<String> menuItems = new ArrayList<String>(Arrays.asList(
+        "1) Print All Recipes", "2) Print Recipe Details", "3) Exit"));
         
+        do {
+            int userSelection = 0;
+            menuItems.forEach(itemsList -> {
+                System.out.println(itemsList);
+            });                                             //Reads Menu to User
+            
+            try
+            {
+                userSelection = userInput.nextInt();
+            } catch(InputMismatchException e) {
+                System.out.print("Error: Please enter a number within the bounds of the menu.");
+                return -1;
+            } finally {
+                if(userSelection == 3) exit = true;
+                return -1;
+            }
+        } while(!exit);
+    }
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        Ingredient newIngredient = new Ingredient();
+        Recipe newRecipe = new Recipe();
+        
+        System.out.println("Type name of ingredient.");
+        newIngredient.addIngredient(userInput.nextLine());
     }
 }
